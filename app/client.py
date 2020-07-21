915350ece067f67b73411bff3027e6ca98e80927
mlkbk <mlkbk@local.hp>
2020-07-21 06:54:47 +0000

    bettter doc

--- app/client.py
+++ app/client.py
@@ -7,24 +7,24 @@ from flask import render_template, redirect, request
 from app import app
 
 # voir ici pour ngrok
-ADRESSE_NOEUD_CONNECTE = "http://127.0.0.1:8000"
+ADRESSE_NOEUD_SERVEUR = "http://127.0.0.1:8000"
 
 
 @app.route("/")
 def index():
-    reponse = requests.get(f"{ADRESSE_NOEUD_CONNECTE}/info_chaine")
+    reponse = requests.get(f"{ADRESSE_NOEUD_SERVEUR}/info_chaine")
 
     if reponse.status_code == 200:
         info_chaine = json.loads(reponse.content)
         blockchain = info_chaine["chaine"]
         blockchain = sorted(blockchain, key=lambda k: k["timestamp"], reverse=True)
         for i, bloc in enumerate(blockchain):
-            bloc['timestamp'] = litteral_timestamp(bloc['timestamp'])
+            bloc["timestamp"] = litteral_timestamp(bloc["timestamp"])
             blockchain[i] = bloc
 
         contexte = {
-            "titre": "Réseau décentralisé, pour partager du contenu",
-            "adresse_noeud": ADRESSE_NOEUD_CONNECTE,
+            "titre": "Blockchain: un réseau décentralisé pour partager du contenu",
+            "adresse_noeud": ADRESSE_NOEUD_SERVEUR,
             "temps_litteral": litteral_timestamp,
             "pairs": info_chaine["pairs"],
             "lg_chaine": len(info_chaine["chaine"]),
@@ -38,7 +38,7 @@ def index():
 def soumettre_zone_texte():
 
     requests.post(
-        f"{ADRESSE_NOEUD_CONNECTE}/nvl_tx",
+        f"{ADRESSE_NOEUD_SERVEUR}/nvl_tx",
         json={"auteur": request.form["auteur"], "contenu": request.form["contenu"],},
         headers={"Content-type": "application/json"},
     )
@@ -49,13 +49,12 @@ def soumettre_zone_texte():
 @app.route("/senregistrer", methods=["POST"])
 def envoyer_demande_enregistrement():
     requests.post(
-        f"{ADRESSE_NOEUD_CONNECTE}/senregistrer_aupres",
+        f"{ADRESSE_NOEUD_SERVEUR}/senregistrer_aupres",
         json={"adresse": request.form["adresse_denregistrement"]},
         headers={"Content-type": "application/json"},
     )
 
     return redirect("/")
-
 
 
 def litteral_timestamp(temps_):
